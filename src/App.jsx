db25610638b5e3c23af2849daf87ba5e5b9f567d
Dhruv Yalamanchi <dy2444@columbia.edu>
2025-04-22 00:55:58 -0400

    Add Nunito font to everything

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,13 @@ import ShutterSpeed from "./components/ShutterSpeed";
 import ISO from "./components/ISO";
 import CheatSheet from "./components/CheatSheet";
 import CheatSheetDetail from "./components/cheatsheet/CheatSheetDetail";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import PortraitSettings from "./components/cheatsheet/PortraitSettings";
 
 import QuizQuestion from "./components/quiz/QuizQuestion";
 import QuizResults from "./components/quiz/QuizResults";
 
+import "./App.css";
 
 function App() {
   return (
@@ -34,7 +35,6 @@ function App() {
         <Route path="/learn/practice/:type" element={<QuizQuestion />} />
         <Route path="/learn/practice/results" element={<QuizResults />} />
         <Route path="/learn/portrait" element={<PortraitSettings />} />
-
       </Routes>
     </div>
   );
